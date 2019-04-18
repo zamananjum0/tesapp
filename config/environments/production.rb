7c795448a16a5e5f12a4a42829a676cbb118e089
Usama Ashraf <usama.ashraf.khan@gmail.com>
2019-04-18 13:53:40 +0500

    make it to live to the w

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.action_mailer.default_url_options = { host: 'https://github-prapp.herokuapp.com' }
 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
@@ -84,6 +83,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.action_mailer.default_url_options = { host: 'https://github-prapp.herokuapp.com' }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
